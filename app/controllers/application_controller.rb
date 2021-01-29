ca8558221cb606a05526fbff308d93387474c744
David Moina <davemolina777@gmail.com>
2021-01-28 22:07:15 -0600

    commit tree c76751c39e1f94c84a93b6bd27a9d49898426d2f

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,42 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
-
+  #index
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+  #New
+  get '/recipes/new' do
+    erb :new
+  end
+  post '/recipes' do
+   # binding.pry
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+  #show
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+  #edit
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+  #delete
+  delete '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to "/recipes"
+  end
 end
